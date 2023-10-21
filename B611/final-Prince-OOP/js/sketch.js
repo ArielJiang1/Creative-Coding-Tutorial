1a2fe454b422a8e315bb9d44f4cf7d1e59610971
Carrot Xinyu Liu <xl3138@nyu.edu>
2023-10-21 12:35:36 +0800

    princescarf

--- B611/final-Prince-OOP/js/sketch.js
+++ B611/final-Prince-OOP/js/sketch.js
@@ -9,63 +9,82 @@ function draw() {
   translate(width / 2, height / 2);
   
   //hair
-  let x = map(sin(frameCount * 0.01), -1, 1, -20, 20);
-  let y = map(cos(frameCount * 0.01), -1, 1, -100, -90);
+  let hairX = map(sin(frameCount * 0.01), -1, 1, -20, 20);
+  let hairY = map(cos(frameCount * 0.01), -1, 1, -100, -90);
+  
+  let scarfFluctY = map(noise(sin(frameCount * 0.03)), 0, 1, -10, 10);
+  
   
   strokeWeight(2);
   stroke(125, 206, 19);
   noFill();
-  bezier(x, y, 0, -70, 0, -30, 0, -30);
+  bezier(hairX, hairY, 0, -70, 0, -30, 0, -30);
   
   //head
   for(i = 0; i < 100; i++){
     noStroke();
     fill(244,206,20,10-floor(map(i, 0, 99, 5, 0)));
-    circle(x,y,i * 0.35);
+    circle(hairX,hairY,i * 0.35);
     fill(255,35-floor(map(i, 0, 99, 5, 0)));
-    // stroke(244, 206, 20, 50);
-    // strokeWeight(1);
     ellipse(0,0, 25+i); 
   }
   fill(244,206,10);
-  circle(x,y, 15);
+  circle(hairX,hairY, 15);
   stroke(0);
   strokeWeight(5);
+  
+  
+  //eyes
   let leftOffsetX = map(mouseX, 0, 200, -10, 0);
   let rightOffsetX = map(mouseX, 200, 400, 0, 10);
   if(openEye == 1){
     if(mouseX > 200){
-      line(-15 + rightOffsetX, -3, -15 + rightOffsetX, 3);
-      line(15 + rightOffsetX, -3, 15 + rightOffsetX, 3);
+      line(-16 + rightOffsetX, -3, -16 + rightOffsetX, 3);
+      line(16 + rightOffsetX, -3, 16 + rightOffsetX, 3);
     } else{
-      line(-15 + leftOffsetX, -3, -15 + leftOffsetX, 3);
-      line(15 + leftOffsetX, -3, 15 + leftOffsetX, 3);
+      line(-16 + leftOffsetX, -3, -16 + leftOffsetX, 3);
+      line(16 + leftOffsetX, -3, 16 + leftOffsetX, 3);
     }
     
   } else{
     if(mouseX > 200){
-      line(-19 + rightOffsetX, -1.5, -13 + rightOffsetX, -1.5);
-      line(19 + rightOffsetX, -1.5, 13 + rightOffsetX, -1.5);
+      line(-18 + rightOffsetX, -1.5, -13 + rightOffsetX, -1.5);
+      line(18 + rightOffsetX, -1.5, 13 + rightOffsetX, -1.5);
     }else{
-      line(-19 + leftOffsetX, -1.5, -13 + leftOffsetX, -1.5);
-      line(19 + leftOffsetX, -1.5, 13 + leftOffsetX, -1.5);
+      line(-18 + leftOffsetX, -1.5, -13 + leftOffsetX, -1.5);
+      line(18 + leftOffsetX, -1.5, 13 + leftOffsetX, -1.5);
     }
   }
-  // bezier(-50, 35, -10, 45, 10, 45, 50, 35);
+  
+  
+  //scarf2
+  push();
+  noStroke();
+  fill(216,180,3);
+  beginShape();
+  vertex(55, 28);
+  bezierVertex(85, 20 , 85, 20 , 150,22);
+  vertex(138, 60);
+  bezierVertex(85, 45 , 85, 45 , 50,45);
+  endShape();
+  pop();
   
   //scarf1
   noStroke();
-  fill("#F4CE14");
+  fill(244,206,10);
   beginShape();
-  vertex(-50, 35);
-  bezierVertex(-10, 45, 10, 45, 50, 35);
-  vertex(50, 60);
-  bezierVertex(10, 70, -10, 70, -50, 60);
+  vertex(-55, 28);
+  bezierVertex(-15, 40, 15, 40, 55, 28);
+  vertex(55, 53);
+  bezierVertex(15, 65, -15, 65, -55, 53);
   endShape();
   
  
 }
 function keyPressed(){
   openEye *= -1;
+  if (key === "s") {
+    saveGif("prince-1.1", 3);
+  }
 }
 
