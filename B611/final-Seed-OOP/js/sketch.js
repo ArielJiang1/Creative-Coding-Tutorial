4bc5c1c62d9b906e59e361dbe17facaa8ff5e78c
Carrot Xinyu Liu <xl3138@nyu.edu>
2023-10-24 17:06:10 +0800

    seed1

--- B611/final-Seed-OOP/js/sketch.js
+++ B611/final-Seed-OOP/js/sketch.js
@@ -18,18 +18,31 @@ let colorRange = [
     [234, 253, 252],
   ],
 ];
+let layerNum = 5;
+let seeds = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  for(let r = 0; r <= layerNum; r ++){
+    for(let i = 0; i < 2 * PI; i += (2 * PI) / (11 + r * 3)){
+      seeds.push(new Seed(width / 2, height / 2, r, i, 1, 1, 0));
+    }
+    
+
+  }
 }
 
-function draw() {}
+function draw() {
+
+}
 
 class Seed {
-  constructor(x, y, i, l, dirx, diry, dia, ci) {
+  constructor(x, y, layer, sdPos, l, dirx, diry, ci) {
     this.x = x;
     this.y = y;
+    this.layer = layer;
+    this.seedPos = sdPos;
     this.xSpd = 0;
     this.ySpd = 0;
     this.angle = i;
@@ -39,21 +52,19 @@ class Seed {
     this.dia = dia;
 
     this.ifFly = false;
-    this.alpha = 255;
-    this.changeA = 0;
+
+    this.colorIndex = ci;
   }
 
   update() {
     this.x += this.xSpd;
     this.y += this.ySpd;
-    this.alpha -= this.changeA;
   }
 
   display() {
     push();
     translate(this.x, this.y);
-    rotate(this.angle);
-    this.drawSeed(this.l, this.dia, this.alpha);
+    this.drawSeed();
     pop();
   }
 
@@ -69,22 +80,46 @@ class Seed {
   fly(dirx, diry) {
     this.xSpd += random(-0.01, 0.01) + random(0, dirx);
     this.ySpd += random(-0.01, 0.01) - random(0, diry);
-    this.changeA = 0.235;
   }
 
-  drawSeed(l, dia, a, ci) {
+  drawSeed() {
+    push();
+    let x1 = sin((PI / 2) * (this.layer + 1) + this.seedPos) * (40 + this.layer * 20);
+    let y1 = cos((PI / 2) * (this.layer + 1) + this.seedPos) * (40 + this.layer * 20);
+    fill(
+      map(fluct, -1, 1, colorRange[this.colorIndex][0][0], colorRange[this.colorIndex][1][0]),
+      map(fluct, -1, 1, colorRange[this.colorIndex][0][1], colorRange[this.colorIndex][1][1]),
+      map(fluct, -1, 1, colorRange[this.colorIndex][0][2], colorRange[this.colorIndex][1][2])
+    );
+    circle(x1, y1, map(sin(i + frameCount * 0.05), -1, 1, 3, 6 + this.layer * 3.5));
+    pop();
+  }
+}
+
+class Core{
+  constructor(x, y, ci){
+    this.x = x;
+    this.y = y;
+    this.colorIndex = ci;
+  }
+
+  display(){
+    push();
+    translate(this.x, this.y);
+    pop();
+
+  }
+
+  update(){
+
+  }
+
+  drawStem(){
     push();
     strokeWeight(3);
-    stroke(colorStem[ci][0], colorStem[ci][1], colorStem[ci][2], a);
+    stroke(colorStem[this.colorIndex][0], colorStem[this.colorIndex][1], colorStem[this.colorIndex][2], a);
     line(0, 0, 0, l);
     noStroke();
-    fill(
-      map(fluct, -1, 1, colorRange[ci][0][0], colorRange[ci][1][0]),
-      map(fluct, -1, 1, colorRange[ci][0][1], colorRange[ci][1][1]),
-      map(fluct, -1, 1, colorRange[ci][0][2], colorRange[ci][1][2]),
-      a
-    );
-    circle(0, l, dia);
     pop();
   }
 }
